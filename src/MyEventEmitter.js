024a2bf99d862de20a13337eb3a3b3fef29ccb75
Mykhailo <kurochkinmy@gmail.com>
2025-09-09 14:45:36 +0300

    feat: implement custom event emitter

--- src/MyEventEmitter.js
+++ src/MyEventEmitter.js
@@ -1,14 +1,64 @@
 'use strict';
 
 class MyEventEmitter {
-  on() {}
-  once() {}
-  off() {}
-  emit() {}
-  prependListener() {}
-  prependOnceListener() {}
-  removeAllListeners() {}
-  listenerCount() {}
+  listeners = {};
+
+  on(eventName, callback) {
+    if (!this.listeners[eventName]) {
+      this.listeners[eventName] = [];
+    }
+    this.listeners[eventName].push(callback);
+  }
+  once(eventName, callback) {
+    if (!this.listeners[eventName]) {
+      this.listeners[eventName] = [];
+    }
+
+    const onceWrapper = (...args) => {
+      this.off(eventName, onceWrapper);
+      callback(...args);
+    };
+
+    this.listeners[eventName].push(onceWrapper);
+  }
+  off(eventName, callback) {
+    this.listeners[eventName] = this.listeners[eventName].filter(
+      (item) => item !== callback,
+    );
+  }
+  emit(eventName, ...args) {
+    for (const callback of this.listeners[eventName]) {
+      callback(...args);
+    }
+  }
+  prependListener(eventName, callback) {
+    if (!this.listeners[eventName]) {
+      this.listeners[eventName] = [];
+    }
+    this.listeners[eventName].unshift(callback);
+  }
+  prependOnceListener(eventName, callback) {
+    if (!this.listeners[eventName]) {
+      this.listeners[eventName] = [];
+    }
+
+    const onceWrapper = (...args) => {
+      this.off(eventName, onceWrapper);
+      callback(...args);
+    };
+
+    this.listeners[eventName].unshift(onceWrapper);
+  }
+  removeAllListeners(eventName) {
+    if (eventName) {
+      this.listeners[eventName] = [];
+    } else {
+      this.listeners = {};
+    }
+  }
+  listenerCount(eventName) {
+    return eventName in this.listeners ? this.listeners[eventName].length : 0;
+  }
 }
 
 module.exports = MyEventEmitter;
